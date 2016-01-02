4d3a560413f4ce79b3da5ffbf94a5e1772e02a51
Christian Budde Christensen <christi@n-bud.de>
2016-01-02 22:24:31 +0100

    Initial push

---
+++ test/lib/util/MailMySQLConstantsImpl.php
@@ -0,0 +1,19 @@
+<?php
+namespace ChristianBudde\PartMail\util;
+use ChristianBudde\Part\test\util\MySQLConstantsImpl;
+
+/**
+ * Created by PhpStorm.
+ * User: budde
+ * Date: 7/4/14
+ * Time: 11:53 PM
+ */
+
+
+class MailMySQLConstantsImpl extends MySQLConstantsImpl
+{
+    function __construct()
+    {
+        parent::__construct($GLOBALS['DB_MAIL_HOST'], $GLOBALS['DB_MAIL_NAME'], $GLOBALS['DB_MAIL_USER'], $GLOBALS['DB_MAIL_PASSWORD']);
+    }
+}
